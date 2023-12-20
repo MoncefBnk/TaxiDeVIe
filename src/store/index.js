ae9c6a23c923204e50bb18ae8823e05feb3a072d
Moncyyy <moncef-benkhalfallah@outlook.com>
2023-12-20 14:37:11 +0100

    ProfilePage Display

--- src/store/index.js
+++ src/store/index.js
@@ -3,19 +3,30 @@ import router from '@/router';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { auth } from '@/firebase';
 import axios from 'axios';
+import createPersistedState from 'vuex-persistedstate';
 
 export default createStore({
+  plugins: [createPersistedState()],
   state: {
-    user: null
+    user: null,
+    numberClient: null,
+    clientInfo: {},
   },
   mutations: {
     SET_USER(state, user) {
       state.user = user;
+      state.numberClient = user ? user.uid : null;
     },
 
     CLEAR_USER(state) {
       state.user = null;
-    }
+    },
+    SET_NUMBER_CLIENT(state, numberClient) {
+      state.numberClient = numberClient;
+    },
+    SET_CLIENT_INFO(state, clientInfo) {
+      state.clientInfo = clientInfo;
+    },
   },
   actions: {
     async login({ commit }, { email, password }) {
@@ -40,20 +51,20 @@ export default createStore({
 
     async registerUserWithAPI({ commit }, userData) {
       try {
-        
+
         await axios.post('https://localhost:7066/v1/api/Client', userData, {
           headers: {
             'Content-Type': 'application/json',
           },
         });
-  
-        
+
+
         commit('SET_USER', auth.currentUser);
         router.push('/dashboard');
       } catch (error) {
         console.error('Error registering user with API:', error.message);
-        
-        
+
+
       }
     },
 
@@ -61,6 +72,21 @@ export default createStore({
       await signOut(auth);
       commit('CLEAR_USER');
       router.push('/');
+    },
+
+
+    async fetchClientInfo({ commit }, numberClient) {
+      try {
+        if (numberClient !== null && numberClient !== undefined) {
+          const response = await axios.get(`https://localhost:7066/v1/api/Client/byNumberClient?numberClient=${numberClient}`);
+          const clientInfo = response.data;
+          commit('SET_CLIENT_INFO', clientInfo);
+        } else {
+          console.error('Invalid numberClient:', numberClient);
+        }
+      } catch (error) {
+        console.error('Error fetching client info:', error.message);
+      }
     },
 
     async fetchUser({ commit }, to) {
@@ -72,8 +98,10 @@ export default createStore({
           }
         } else {
           commit('SET_USER', user);
-        
-          if (router.isReady() && to && (to.name === 'Login' || to.name === 'ForgotPassword'|| to.name === 'Home'||to.name ==='Register')) {
+
+          commit('SET_NUMBER_CLIENT', user.uid);
+
+          if (router.isReady() && to && (to.name === 'Login' || to.name === 'ForgotPassword' || to.name === 'Home' || to.name === 'Register')) {
             router.push({ name: 'Dashboard' });
           }
         }
@@ -81,7 +109,7 @@ export default createStore({
     }
   }
 });
-   
+
 function handleAuthError(error) {
   const errorMessages = {
     'auth/user-not-found': 'User not found',
